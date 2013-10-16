fe517a710a61c1bc3d7afde6239f1a7767a867af
Fa[n]tastik <dmitry.koryakin@gmail.com>
2013-10-16 23:59:10 +0300

    update func f_sum_benchmark in lab 2

--- Lab 2/Scripts/f_sum_benchmark.sql
+++ Lab 2/Scripts/f_sum_benchmark.sql
@@ -1,1 +1,25 @@
-﻿SELECT f_sum_benchmark();
+﻿-- Function: f_sum_benchmark()
+
+-- DROP FUNCTION f_sum_benchmark();
+
+CREATE OR REPLACE FUNCTION f_sum_benchmark()
+  RETURNS void AS
+$BODY$
+DECLARE
+  ts_start timestamp;
+BEGIN
+  ts_start := clock_timestamp();
+  PERFORM SUM(sal) FROM idx;
+  RAISE NOTICE 'PERFORM SUM(sal) FROM idx;';
+  RAISE NOTICE 'Duration: %', clock_timestamp() - ts_start;
+
+  ts_start := clock_timestamp();
+  PERFORM f_sum_by_cursor();
+  RAISE NOTICE 'SELECT f_sum_by_cursor();';
+  RAISE NOTICE 'Duration: %', clock_timestamp() - ts_start; 
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION f_sum_benchmark()
+  OWNER TO fantastik;
